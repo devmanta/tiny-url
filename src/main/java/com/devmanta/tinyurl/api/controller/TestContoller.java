ccf9f9e96318013c172542d4da05fb6031c561b5
heejin <dlwlrgkwk82@gmail.com>
2025-03-14 00:41:56 +0900

    chore: set test redis value to current time

--- src/main/java/com/devmanta/tinyurl/api/controller/TestContoller.java
+++ src/main/java/com/devmanta/tinyurl/api/controller/TestContoller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
@@ -20,7 +21,7 @@ public class TestContoller {
 
     @GetMapping("/set")
     public String setRedisData() {
-        redisService.setValues("mykey", "myvalue");
+        redisService.setValues("mykey", LocalDateTime.now().toString());
         return "Success";
     }
 
